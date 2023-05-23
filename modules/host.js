5173838fc1f65466c9c0d462161c164b0bda5173
Frank Lemanschik <117739566+lemanschik@users.noreply.github.com>
2023-05-23 18:46:30 +0200

    Update host.js

--- modules/host.js
+++ modules/host.js
@@ -1,36 +1,63 @@
 const net = require('node:net');
 const repl = require('node:repl');
 const fs = require('node:fs/promises');
-
+const { Duplex, Writable, Readable } = require('node:stream')
 const path = process.argv.find(a=>a.startsWith('PATH=')) || process.cwd;
+const peers = [];
+const RTCPeerConnection = () => {
+      const peer = peers[peers.push({})-1];
+      peer.ondatachannel = async (channel) => {
+            const [_replServer,{ readable }] = tryConnect(path); 
+            const input = writable.getWriter();
+            channel.onmessage = (msg) => input.write(msg); // replServer.input.push bad idea none generiic?
+            
+            
+            
+            for await (const output of readable) {
+                  channel.postMessage(output);
+            }            
+      }
+}
+
+//Duplex.toWeb(replListen(Duplex.fromWeb(
+// Readable.toWeb(process.stdin),
+// Writable.toWeb(process.stdout)
+//)))
 
 const replListen = (socket) => {
-      connections += 1;
-      useGlobal: true,
-      repl.start({
+      replListen.connections = (replListen.connections || 0) += 1;
+      return repl.start({
+        useGlobal: true,
         prompt: 'Node.js via Unix socket> ',
         input: socket,
         output: socket,
       }).on('exit', () => {
         socket.end();
       });
-    }
-
-const listen = async () => {
-  let connections = 0;  
-  await fs.readFile(`${path}.sock`).then(
-    () => [net.createConnection({path})].map(replListen)),
-    fs.unlink(`${path}.sock`)).then(()=>{
-      net.createServer(replListen).listen(`${path}.sock`);
-    })
 }
 
-const init = () => fs.readdir(path).then(
+const resetSocket = (path) => () => fs.unlink(`${path}.sock`)).then(()=>{
+      net.createServer(replListen).listen(`${path}.sock`);
+      return Duplex.toWeb(net.createConnection({path}));
+})
+
+const tryConnect = (path) => () => [replListen(net.createConnection({path}))].map(async (connection) => (
+await Duplex.toWeb(connection.output).readable.getReader().read()
+).value.startsWith('Node.js') && Duplex.toWeb(connection.output)).find(connection=>connection) || resetSocket(path);
+
+const listen = async (path) => 
+await fs.readFile(`${path}.sock`).then(
+tryConnect(`${path}.sock`), resetSocket(`${path}.sock`)
+);
+
+const init = (path) => fs.readdir(path).then(
   listen, _doesNotExist => {
     return fs.mkdir(path,{recursive:true}).then(listen);
   }.catch(console.error)
-)
+);
 
-repl.start({ useGlobal: true, prompt: 'Node.js via stdin> ', input: process.stdin, output: process.stdout, });
+const [_replServer,stream] = init();
+Readable.toWeb(process.stdin).pipeTo(stream.writeable);
 
-process.on('exit',() => fs.unlink(`${path}.sock`))
+// repl.start({ useGlobal: true, prompt: 'Node.js via stdin> ', input: process.stdin, output: process.stdout, });
+// process.on('exit',() => fs.unlink(`${path}.sock`))
