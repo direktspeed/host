c88776766817d74d52ad8642269a1f47b75242cf
Frank Lemanschik <117739566+lemanschik@users.noreply.github.com>
2023-05-23 14:41:47 +0200

    Update host.js

--- modules/host.js
+++ modules/host.js
@@ -1,22 +1,40 @@
 const net = require('node:net');
 const repl = require('node:repl');
-let connections = 0;
+const fs = require('node:fs/promises');
 
-repl.start({
-  useGlobal: true,
-  prompt: 'Node.js via stdin> ',
-  input: process.stdin,
-  output: process.stdout,
-});
+const path = process.argv.find(a=>a.startsWith('PATH=')) || process.cwd;
 
-net.createServer((socket) => {
-  connections += 1;
-  useGlobal: true,
+
+const listen = ()=>{
+  let connections = 0;
+
   repl.start({
-    prompt: 'Node.js via Unix socket> ',
-    input: socket,
-    output: socket,
-  }).on('exit', () => {
-    socket.end();
+    useGlobal: true,
+    prompt: 'Node.js via stdin> ',
+    input: process.stdin,
+    output: process.stdout,
   });
-}).listen(`${process.argv.find(a=>a.startsWith('PATH=')) || process.cwd}.sock`);
+
+  net.createServer((socket) => {
+    connections += 1;
+    useGlobal: true,
+    repl.start({
+      prompt: 'Node.js via Unix socket> ',
+      input: socket,
+      output: socket,
+    }).on('exit', () => {
+      socket.end();
+    });
+  }).listen(`${}.sock`);  
+  }
+
+const init = () => fs.readdir(path).then(
+  exists => listen(),
+  async doesNotExist => {
+    return fs.mkdir(path,{recursive:true}).then(init);
+  }
+)
+
+
+
+}
